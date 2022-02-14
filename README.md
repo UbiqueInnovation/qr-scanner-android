4f4ab5a007eceaac17ba26ce683bb9ac34a168f4
Mike Wong <wong@ubique.ch>
2022-02-14 14:12:11 +0100

    Initial project structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# QR Code Scanner for Android
+
+## License
+
+This project is licensed under the terms of the MPL 2 license. See the [LICENSE](LICENSE) file for details.
---
+++ example/src/main/java/ch/ubique/qrscanner/example/MainActivity.kt
@@ -0,0 +1,12 @@
+package ch.ubique.qrscanner.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ch.ubique.qrscanner.R
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
